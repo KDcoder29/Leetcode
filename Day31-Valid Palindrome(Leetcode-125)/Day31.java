3f47475ab6337ae0530856f679b858112c42c6b6
KDcoder29 <123066499+KDcoder29@users.noreply.github.com>
2024-06-10 20:59:42 +0530

    Create Day31.java

---
+++ Day31-Valid Palindrome(Leetcode-125)/Day31.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right) {
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+            
+            if (!Character.isLetterOrDigit(l)) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(r)) {
+                right--;
+                continue;
+            }
+            
+            if (l != r) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
