46ad4849cc02335a981781f619f149c29bed5f9d
KDcoder29 <123066499+KDcoder29@users.noreply.github.com>
2024-06-28 17:12:37 +0530

    Create Day46.java

---
+++ Day46 Binary tree level order traversal II (Leetcode-107)/Day46.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> major = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root==null) return major;
+        q.add(root);
+
+        while(q.size()>0){
+            int n = q.size();
+            List<Integer> minor = new LinkedList<>();
+            for(int i=1;i<=n;i++){
+                TreeNode remove = q.remove();
+                minor.add(remove.val);
+                if(remove.left!=null) q.add(remove.left);
+                if(remove.right!=null) q.add(remove.right);
+            }
+            major.add(0,minor);
+        }
+    return major;
+    }
+}
