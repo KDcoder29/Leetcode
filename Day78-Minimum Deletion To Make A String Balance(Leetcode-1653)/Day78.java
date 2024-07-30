0c9fa166d9dccb408aec7bdad357a12c925001bd
KDcoder29 <123066499+KDcoder29@users.noreply.github.com>
2024-07-30 20:04:06 +0530

    Update Day78.java

--- Day78-Minimum Deletion To Make A String Balance(Leetcode-1653)/Day78.java
+++ Day78-Minimum Deletion To Make A String Balance(Leetcode-1653)/Day78.java
@@ -1,23 +1,16 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int n = s.length();
-        int[] prefixB = new int[n + 1];  
-        int[] suffixA = new int[n + 1];  
-        
-        for (int i = 0; i < n; i++) {
-            prefixB[i + 1] = prefixB[i] + (s.charAt(i) == 'b' ? 1 : 0);
+        int ans =0;
+        int count =0;
+        char[] arr = s.toCharArray();
+        for(char i : arr){
+            if(i=='b')
+            count++;
+            else if(count>0){
+                ans++;
+                count--;
+            }
         }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            suffixA[i] = suffixA[i + 1] + (s.charAt(i) == 'a' ? 1 : 0);
-        }
-        
-        int minDeletions = Integer.MAX_VALUE;
-        
-        for (int i = 0; i <= n; i++) {
-            minDeletions = Math.min(minDeletions, prefixB[i] + suffixA[i]);
-        }
-        
-        return minDeletions;
+    return ans;
     }
 }
